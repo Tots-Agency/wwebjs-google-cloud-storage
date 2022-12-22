73874db2509e31cfe784cfaf455a6afa3d7d3603
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-22 20:49:37 -0300

    Return file path.

--- src/WwebjsCloudStorage.js
+++ src/WwebjsCloudStorage.js
@@ -24,8 +24,8 @@ class WwebjsCloudStorage {
         await this.#deletePrevious(options);
 
         await new Promise((resolve, reject) => {
-            fs.createReadStream(this.getSessionFileName(options))
-                .pipe(this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).createWriteStream())
+            fs.createReadStream(`${options.session}.zip`)
+                .pipe(this.storage.bucket(this.bucketName).file(`${options.session}.zip`).createWriteStream())
                 .on('error', err => reject(err))
                 .on('close', () => resolve());
         });
@@ -33,7 +33,7 @@ class WwebjsCloudStorage {
 
     async extract(options) {
         return new Promise((resolve, reject) => {
-            this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).createReadStream()
+            this.storage.bucket(this.bucketName).file(`${options.session}.zip`).createReadStream()
                 .pipe(fs.createWriteStream(options.path))
                 .on('error', err => reject(err))
                 .on('close', () => resolve());
@@ -42,27 +42,23 @@ class WwebjsCloudStorage {
 
     async delete(options) {
         if(await this.internalExistFile(options)){
-            await this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).delete();
+            await this.storage.bucket(this.bucketName).file(`${options.session}.zip`).delete();
         } 
     }
 
     async #deletePrevious(options) {
         if(await this.internalExistFile(options)){
-            await this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).delete();
+            await this.storage.bucket(this.bucketName).file(`${options.session}.zip`).delete();
         }
     }
 
     async internalExistFile(options) {
         try {
-            let response = await this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).get();
+            let response = await this.storage.bucket(this.bucketName).file(`${options.session}.zip`).get();
             return true;
         } catch (error) {
             return false;
         }
-    }
-
-    getSessionFileName(options) {
-        return this.clientId + '-session.zip';
     }
 }
 
