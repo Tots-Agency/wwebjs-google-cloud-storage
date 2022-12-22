ae844ef88bc89f9ba4bc2c2e22a4a9eabd72a206
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-22 20:41:06 -0300

    Fix function.

--- src/WwebjsCloudStorage.js
+++ src/WwebjsCloudStorage.js
@@ -24,8 +24,8 @@ class WwebjsCloudStorage {
         await this.#deletePrevious(options);
 
         await new Promise((resolve, reject) => {
-            fs.createReadStream(getSessionFileName(options))
-                .pipe(this.storage.bucket(this.bucketName).file(getSessionFileName(options)).createWriteStream())
+            fs.createReadStream(this.getSessionFileName(options))
+                .pipe(this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).createWriteStream())
                 .on('error', err => reject(err))
                 .on('close', () => resolve());
         });
@@ -33,7 +33,7 @@ class WwebjsCloudStorage {
 
     async extract(options) {
         return new Promise((resolve, reject) => {
-            this.storage.bucket(this.bucketName).file(getSessionFileName(options)).createReadStream()
+            this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).createReadStream()
                 .pipe(fs.createWriteStream(options.path))
                 .on('error', err => reject(err))
                 .on('close', () => resolve());
@@ -42,19 +42,19 @@ class WwebjsCloudStorage {
 
     async delete(options) {
         if(await this.internalExistFile(options)){
-            await this.storage.bucket(this.bucketName).file(getSessionFileName(options)).delete();
+            await this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).delete();
         } 
     }
 
     async #deletePrevious(options) {
         if(await this.internalExistFile(options)){
-            await this.storage.bucket(this.bucketName).file(getSessionFileName(options)).delete();
+            await this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).delete();
         }
     }
 
     async internalExistFile(options) {
         try {
-            let response = await this.storage.bucket(this.bucketName).file(getSessionFileName(options)).get();
+            let response = await this.storage.bucket(this.bucketName).file(this.getSessionFileName(options)).get();
             return true;
         } catch (error) {
             return false;
