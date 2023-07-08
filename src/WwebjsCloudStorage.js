2a1216567de20da2a695ef6d170e3524865d6fd8
Matias Camiletti <matias.camiletti@gmail.com>
2023-07-08 13:10:07 -0300

    Add listener and console logs.

--- src/WwebjsCloudStorage.js
+++ src/WwebjsCloudStorage.js
@@ -8,57 +8,91 @@ class WwebjsCloudStorage {
     bucketName = '';
     clientId = '';
     storage;
+    onSaved;
+    isDebug = false;
 
-    constructor(clientId, keyFile, bucketName) {
+    constructor(clientId, keyFile, bucketName, saved = undefined, isDebug = false) {
         this.clientId = clientId;
         this.storageKey = keyFile;
         this.bucketName = bucketName;
         this.storage = new Storage({keyFilename: this.storageKey});
+        this.onSaved = saved;
+        this.isDebug = isDebug;
+        this.printConsole('wwebjs-google-cloud-storage Init');
     }
 
     async sessionExists(options) {
+        this.printConsole(options.session + ' wwebjs-google-cloud-storage Session exists');
         return await this.internalExistFile(options);
     }
 
     async save(options) {
         await this.#deletePrevious(options);
 
+        this.printConsole(options.session + ' wwebjs-google-cloud-storage Session save');
         await new Promise((resolve, reject) => {
             fs.createReadStream(`${options.session}.zip`)
                 .pipe(this.storage.bucket(this.bucketName).file(`${options.session}.zip`).createWriteStream())
-                .on('error', err => reject(err))
-                .on('close', () => resolve());
+                .on('error', err => {
+                    this.printConsole(options.session + ' wwebjs-google-cloud-storage Session save err');
+                    this.printConsole(err);
+                    return reject(err);
+                })
+                .on('close', () => {
+                    if(this.onSaved !== undefined){
+                        this.onSaved(options);
+                    }
+                    this.printConsole(options.session + ' wwebjs-google-cloud-storage Session save close');
+                    return resolve();
+                });
         });
     }
 
     async extract(options) {
+        this.printConsole(options.session + ' wwebjs-google-cloud-storage Session extract');
+
         return new Promise((resolve, reject) => {
             this.storage.bucket(this.bucketName).file(`${options.session}.zip`).createReadStream()
                 .pipe(fs.createWriteStream(options.path))
-                .on('error', err => reject(err))
+                .on('error', err => {
+                    this.printConsole(options.session + ' wwebjs-google-cloud-storage Session err');
+                    this.printConsole(err);
+                    return reject(err);
+                })
                 .on('close', () => resolve());
         });
     }
 
     async delete(options) {
+        this.printConsole(options.session + ' wwebjs-google-cloud-storage Session delete');
         if(await this.internalExistFile(options)){
             await this.storage.bucket(this.bucketName).file(`${options.session}.zip`).delete();
         } 
     }
 
     async #deletePrevious(options) {
+        this.printConsole(options.session + ' wwebjs-google-cloud-storage Session delete previous');
         if(await this.internalExistFile(options)){
             await this.storage.bucket(this.bucketName).file(`${options.session}.zip`).delete();
         }
     }
 
     async internalExistFile(options) {
+        this.printConsole(options.session + ' wwebjs-google-cloud-storage Session internal exists');
         try {
             let response = await this.storage.bucket(this.bucketName).file(`${options.session}.zip`).get();
             return true;
         } catch (error) {
             return false;
         }
+    }
+
+    printConsole(text) {
+        if(!this.isDebug){
+            return;
+        }
+
+        console.log(text);
     }
 }
 
